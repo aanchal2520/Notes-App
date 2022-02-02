3ce92921309d74a9b637212f7f1afec13fa6e9ef
aanchal2520 <aanchal2520@gmail.com>
2022-02-02 18:40:59 +0530

    Final Push

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
 console.log("Welcome!");
 
+const showNotes = () => {
+    let notes = localStorage.getItem('notes');
+    if (notes == null)
+        notesObj = [];
+    else
+        notesObj = JSON.parse(notes);
+
+    let html = '';
+    notesObj.forEach((element, index) => {
+        html += `<div class="notePack">
+                    <div class="note">
+                        <div id="noteTitle" class="noteTitle">Note ${index + 1}</div>
+                        <div class="noteBody">${element}</div>
+                    </div>
+                    <input id=${index} onclick="deleteNode(this.id)" class="btn" type="button" value="Delete note">
+                </div>`;
+    })
+
+    let notesElem = document.getElementById('all-notes');
+    if (notesObj.length != 0)
+        notesElem.innerHTML = html;
+    else
+        notesElem.innerHTML = ''
+}
+
+showNotes();
+
 const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", () => {
     let textNote = document.getElementById('textNote');
@@ -14,15 +41,20 @@ addBtn.addEventListener("click", () => {
     notesObj.push(textNote.value);
     localStorage.setItem('notes', JSON.stringify(notesObj));
     textNote.value = "";
+    showNotes();
 });
 
-const showNotes = () => {
+function deleteNode(index) {
+    console.log("Delete clicked!")
     let notes = localStorage.getItem('notes');
-    if (notes == null)
+
+    if (notes == null) {
         notesObj = [];
-    else
+    } else {
         notesObj = JSON.parse(notes);
+    }
 
-    let html = '';
-
+    notesObj.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    showNotes();
 }
